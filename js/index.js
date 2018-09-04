8f20d1682a0fba0509135b63f00597608af87f1f
Serhiy Mykytiuk <serhiyshf@bigmir.net>
2018-09-04 18:17:35 +0000

    done2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const n = parseFloat(prompt('Enter N'));
 
 function fibonacci(n) {
-  let sq5 = Math.sqrt(5); 
+  const sq5 = Math.sqrt(5); 
   const a = (1 + sq5) / 2;
   const b = (1 - sq5) / 2;
   return (Math.pow(a, n) - Math.pow(b, n)) / sq5;
